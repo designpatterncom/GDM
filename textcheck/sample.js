70802557adab84bbc414b5f06ff8ab953e4c89fc
viperlike <viperlike2004@yahoo.co.jp>
2014-07-17 12:38:17 +0900

    テキストチェック

---
+++ textcheck/sample.js
@@ -0,0 +1,12 @@
+function func()
+{
+    var text1=document.form1.input1.value;
+    var text2=document.form1.input2.value;
+    var text3=document.form1.input3.value;
+
+    if(text1=="will" && text2=="take" && text3=="off"){
+	alert("OK!");
+    } else {
+	alert("NG!");
+    }
+}
