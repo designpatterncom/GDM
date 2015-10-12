dbe016619d7fea7767ed2220cc62bf2fb319d395
designpatterncom <designpattern.community@gmail.com>
2015-10-13 08:50:51 +0900

    AOJソースコード追加

---
+++ AOJ/ITP1_2_C Sorting Three Numbers/C#/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ITP1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str_List = Console.ReadLine().Split(' ');
+            var list = new List<int>();
+            foreach (string str in str_List)
+            {
+                list.Add(int.Parse(str));
+            }
+            list.Sort();
+
+            Console.Write(list[0]);
+            Console.Write(" ");
+            Console.Write(list[1]);
+            Console.Write(" ");
+            Console.Write(list[2]);
+
+            Console.ReadLine();
+
+        }
+    }
+}
