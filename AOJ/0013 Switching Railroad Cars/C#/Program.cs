139842f63b49c34c69bd7c49507d742e0d29fec8
designpatterncom <designpattern.community@gmail.com>
2016-02-16 11:21:35 +0900

    AOJ C#回答追加

---
+++ AOJ/0013 Switching Railroad Cars/C#/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vol0_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> iStack = new Stack<int>();
+            string sInput = "";
+            // 入力の受け取り
+            do
+            {
+                sInput = Console.ReadLine();
+                if (sInput != null && sInput != "")
+                {
+                    int iInput = int.Parse(sInput);
+                    if (iInput == 0)
+                    {
+                        Console.WriteLine("{0}", iStack.Pop());
+                    }
+                    else
+                    {
+                        iStack.Push(iInput);
+                    }
+                }
+            } while (sInput != null && sInput != "");
+        }
+    }
+}
---
+++ AOJ/0031 Weight/C#/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vol0_31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] iWeight = new int[] { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
+            var str_List = new List<string>();
+            string sInput = "";
+            // 入力の受け取り
+            do
+            {
+                sInput = Console.ReadLine();
+                if (!string.IsNullOrEmpty(sInput)) {
+                    str_List.Add(sInput);
+                }
+            } while (!string.IsNullOrEmpty(sInput));
+
+            foreach(string s in str_List)
+            {
+                int iInput = int.Parse(s);
+                foreach(int i in iWeight)
+                {
+                    if( (iInput & i) == i)
+                    {
+                        Console.Write("{0} ", i);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
