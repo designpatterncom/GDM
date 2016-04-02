719f123dc14f7cd0f62cbe8221b952ce9ff78f1a
designpatterncom <designpattern.community@gmail.com>
2016-04-02 15:15:45 +0900

    C#回答追加

---
+++ AOJ/0003 Is it a Right Triangle？/C#/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vol0_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 一行目の入力が、その後の入力回数となる
+            int iInputNum = int.Parse(Console.ReadLine());
+
+            // 入力にもとづいて、直角三角形かチェック
+            for (int i = 0; i < iInputNum; i++)
+            {
+                List<double> iList = new List<double>();
+                int iParam1 = 0;
+                int iParam2 = 0;
+                int iParam3 = 0;
+                
+                // 入力を受け取り、二乗した値をListに格納
+                string[] str_List = Console.ReadLine().Split(' ');
+                iParam1 = int.Parse(str_List[0]);
+                iParam2 = int.Parse(str_List[1]);
+                iParam3 = int.Parse(str_List[2]);
+
+                // 最大長を算出する
+                iList.Add(System.Math.Pow(iParam1,2));
+                iList.Add(System.Math.Pow(iParam2,2));
+                iList.Add(System.Math.Pow(iParam3,2));
+                iList.Sort();
+                
+                // 直角三角形かチェック
+                double dTmp = iList[0] + iList[1];
+                if (dTmp == iList[2])
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AOJ/0024 Physical Experiments/C#/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vol0_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sInput = "";
+            // 入力の受け取り
+            do
+            {
+                sInput = Console.ReadLine();
+                if (sInput != null && sInput != "")
+                {
+                    double dSpeed = double.Parse(sInput);
+
+                    int N = getN(getY(getTime(dSpeed)));
+
+                    Console.WriteLine("{0}",N);
+                }
+            } while (sInput != null && sInput != "");
+        }
+
+        // 落ちるまでの秒数を求める処理
+        static double getTime(double dSpeed)
+        {
+            double t = dSpeed / 9.8;
+
+            return t;
+        }
+
+        // 距離を求める処理
+        static double getY(double t)
+        {
+            double y = 4.9 * t * t;   // 四捨五入
+
+            return y;
+        }
+
+        // 距離から回数を算出する
+        static int getN( double y )
+        {
+            int N = 0;
+
+            double dtmp = (y + 5) / 5;
+            N = (int)Math.Ceiling( dtmp );
+
+            return N;
+        }
+
+    }
+}
